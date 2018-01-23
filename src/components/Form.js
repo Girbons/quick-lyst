d1a2a043e13d4eb0c71d22be5ff2145626cde4b6
Alessandro De Angelis <alessandrodea22@gmail.com>
2018-01-23 14:44:22 +0100

    Fixed form input

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,14 +4,16 @@ import { Field, reduxForm } from 'redux-form'
 
 
 let TaskForm = props => {
-  const { handleSubmit } = props;
+  const { handleSubmit, submitting, pristine } = props;
   return (
     <form onSubmit={handleSubmit(props.onSubmit)}>
       <div>
         <label htmlFor="new task">New Task</label>
         <Field name="task" component="input" type="text" />
       </div>
-      <button type="submit">Add Task</button>
+      <button type="submit" disabled={pristine || submitting}>
+      Add Task
+     </button>
     </form>
   )
 }
